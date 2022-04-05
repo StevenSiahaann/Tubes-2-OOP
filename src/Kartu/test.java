41ae55bf54f78611a0191c5db261681a94e87a1e
Steven Siahaan <88304745+StevenSiahaann@users.noreply.github.com>
2022-04-06 05:28:43 +0700

    Update test.java

--- src/Kartu/test.java
+++ src/Kartu/test.java
@@ -1,21 +1,5 @@
 import java.util.ArrayList;
 public class test {
-    // private String playerName;
-    // private int playerTurn;
-    // public ArrayList<Kartu> kartu;//kartu tangan
-    // public ArrayList<Kartu> move_card;//move kartu tangan ke board
-    // private Deck dek_kartu= new Deck();//Buat tumpukan kartu semuanya(fungsinya biar nanti pas draw tinggal ngerandom dari ini)
-    // private int mana; //bertambah setiap gilirannya
-    // private int hp;
-
-    // //Constructor Player
-    // public test(String playerName) {
-    //     this.playerName = playerName;
-    //     hp=80;
-    //     int mana=1;
-    //     kartu = new ArrayList<Kartu>();
-    //     move_card = new ArrayList<Kartu>();
-    // }
     public static void main(String[] args) {
         Karakter tes =new Karakter("1", "tesss", "aaa", "aaaaaa", 1, 2, 3,4,5);
         tes.display();
