584f65208fc81adc3a4a5dede219b608ee51a20d
Nathaniel Evan <nathanielevan@zohomail.com>
2022-04-23 04:48:51 +0700

    Add javadoc comments for Kartu.java and Player.java

--- src/Card/Kartu.java
+++ src/Card/Kartu.java
@@ -7,6 +7,9 @@ public class Kartu {
     private String imagepath;
     private String jenis;
 
+    /**
+     * Default class constructor for Kartu.
+     */
     public Kartu(){
         this.ID="0";
         this.Nama="";
@@ -14,6 +17,16 @@ public class Kartu {
         this.imagepath="";
         this.jenis="None";
     }
+
+    /**
+     * Class constructor for Kartu with arguments.
+     *
+     * @param ID The ID for the new card object
+     * @param Nama The name for the new card object
+     * @param Desc The description for the new card object
+     * @param imagepath The image path for the card face
+     * @param jenis The type of the card (character, spell)
+     */
     public Kartu(String ID,String Nama, String Desc , String imagepath, String jenis){
         this.ID=ID;
         this.Nama= Nama;
@@ -28,20 +41,49 @@ public class Kartu {
         System.out.println(this.Desc);
         System.out.println(this.imagepath);
     }
-    //getter
+
+    /**
+     * This method returns the image path of a card object.
+     *
+     * @return String denoting image path of card
+     */
     public String getImagePath(){
         return this.imagepath;
     }
+
+    /**
+     * This method returns the ID of a card object.
+     *
+     * @return String denoting ID of card
+     */
     public String getID(){
         return this.ID;
     }
+
+    /**
+     * This method returns the name of a card object.
+     *
+     * @return String denoting name of card
+     */
     public String getName(){
         return this.Nama;
 
     }
+
+    /**
+     * This method returns the description of a card object.
+     *
+     * @return String denoting description of card
+     */
     public String getDesc(){
         return this.Desc;
     }
+
+    /**
+     * This method returns the card type.
+     *
+     * @return String denoting the card type
+     */
     public String getJenis(){
         return this.jenis;
     }
--- src/Player/Player.java
+++ src/Player/Player.java
@@ -10,6 +10,10 @@ public class Player {
     /* private Deck deck;
      private HandCard hand_card;
      private List<Card> field_card*/
+
+    /**
+     * Default class constructor for Player.
+     */
     public Player(){
         this.nama = "Player " + id;
         id++;
@@ -19,6 +23,14 @@ public class Player {
         // this.hand_card = new HandCard();
         // this.field_card = new ArrayList<Card>();
     }
+
+    /**
+     * Class constructor for Player with arguments.
+     *
+     * @param nama The name for the new player
+     * @param hp The starting HP for the new player
+     * @param mana_remains The starting mana for the new player
+     */
     public Player(String nama,double hp,double mana_remains){ // (Deck deck, HandCard hand_card)
         this.nama = nama;
         this.hp=hp;
@@ -27,12 +39,30 @@ public class Player {
         // this.hand_card = hand_card
         id++;
     }
+
+    /**
+     * This method returns the name of a player.
+     *
+     * @return The name of a player.
+     */
     public String getNama(){
         return this.nama;
     }
+
+    /**
+     * This method returns the HP of a player.
+     *
+     * @return The HP of a player.
+     */
     public double getHp(){
         return this.hp;
     }
+
+    /**
+     * This method returns the remaining mana of a player.
+     *
+     * @return The remaining mana of a player.
+     */
     public double getMana_remains(){
         return this.mana_remains;
     }
@@ -44,15 +74,41 @@ public class Player {
         return this.hand_card;
     }
      */
+
+    /**
+     * This method sets the name of a player.
+     *
+     * @param nama The new name that will be set on a player.
+     */
     public void setNama(String nama){
         this.nama = nama;
     }
+
+    /**
+     * This method sets the HP of a player.
+     *
+     * @param hp New HP that will be set on a player.
+     */
     public void setHp(double hp){
         this.hp = hp;
     }
+
+    /**
+     * This method sets the remaining mana of a player.
+     *
+     * @param mana_remains The remaining mana to set on a player
+     */
     public void setMana_remains(double mana_remains){
         this.mana_remains = mana_remains;
     }
+
+    /**
+     * This method sends an attack from an attacking character to a target player.
+     * This attack will directly reduce the HP of the target player.
+     *
+     * @param Target Player that is targeted by the attack
+     * @param attacker The attacking character
+     */
     public void DirectAttack(Player Target,Karakter attacker){
         if(Target.getHp() < attacker.getAtt()){
             System.out.println(Target.getNama() + " telah terkalahkan\n");
